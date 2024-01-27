8f730b99ec08a8f08e184fc235e237404cf1ad45
smbanati-IZxCKm <smbanati@gmail.com>
2024-01-26 19:19:07 -0500

    setup the starter code, installed inquirer, added packages for the application, created functions for write and initialze

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
+
+
+
+
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+    
+    console.log('✨Success!✨ Enjoy your README 🎉 ')
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((userResponses) => {
+        const markdownContent = generateMarkdown(userResponses);
+        writeToFile('README.md', markdownContent);
+    });
+}
 
 // Function call to initialize app
 init();
